ad52eb5dd36778c6387ba0fdd9beef52d25aa399
Jonathan Soto <jonathan.soto@sigfox.com>
2018-02-21 12:46:22 +0100

    starter

---
+++ start.sh
@@ -0,0 +1,13 @@
+# Download this project
+# Check versions
+docker version
+docker-compose version
+git version
+
+# Checkout the branch
+git clone https://github.com/hellofreshdevtests/jsotogaviard-api-test
+cd jsotogaviard-api-test
+git checkout dev1
+
+# Start docker
+docker-compose up -d --build
